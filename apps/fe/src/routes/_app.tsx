4f17e5066c901e7f172ed0e58393275cadc7b18d
Saket Tawde <saket.tawde@gmail.com>
2026-04-26 22:09:28 +0000

    refactor(fe): name the _app layout component for Fast Refresh stability

--- apps/fe/src/routes/_app.tsx
+++ apps/fe/src/routes/_app.tsx
@@ -1,10 +1,12 @@
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 import { AppShell } from "#/components/app-shell";
 
-export const Route = createFileRoute("/_app")({
-  component: () => (
+function AppLayout() {
+  return (
     <AppShell>
       <Outlet />
     </AppShell>
-  ),
-});
+  );
+}
+
+export const Route = createFileRoute("/_app")({ component: AppLayout });
